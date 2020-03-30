b1b410ca8096c72807e8f59b775de6fcb9875633
Logan Ricard <61986263+L21Studio@users.noreply.github.com>
2020-03-30 10:20:23 -0500

    v1

---
+++ logvnricvrd.js
@@ -0,0 +1,20 @@
+let twitter = document.getElementById('twitter'),
+	instagram = document.getElementById('instagram'),
+	youtube = document.getElementById('youtube'),
+	enterMain = document.getElementById('mainLink');
+
+twitter.onclick = function() {
+	window.open('https://twitter.com/LogvnRicvrd');
+};
+
+instagram.onclick = function() {
+	window.open('https://www.instagram.com/lr.studio.js/');
+};
+
+youtube.onclick = function() {
+	window.open('https://www.youtube.com/channel/UCEJ9Yw2y_YYifTyrW15XmRA?view_as=subscriber');
+};
+
+enterMain.onclick = function() {
+	window.open('../portfolioPage1/index.html');
+};
